89441dfe007fd27e7f1e939d3f9e9b1ead77a381
danroav <danielon83@gmail.com>
2021-08-14 00:38:38 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import "./styles.css";
+//document.getElementById("app").innerHTML = `
+
+//`;
+
+//PREGUNTA :
+//-Para que nos sirve addEventListener?
+//-devuelve el manejador del evento click del elemento actual.
+//-para registra un evento a un objeto en específico(CORRECTA)
+//-elemento para ejecutar un script una vez a la página web se ha cargado completamente todo el contenido
+//-para listar  un evento en  un objeto en específico
+
+/*
+Grupo 2 – Eventos: Eventos del DOM
+	El gran libro de HTML, CSS3 y JavaScript: Cap. 4.4, https://developer.mozilla.org/es/docs/Web/Events
+	Ejemplo de Eventos de Teclado y Mouse, mostrar en elementos HTML el momento en que se ejecutan los eventos.
+	Pregunta para Cuestionario.
+*/
+//
+//1.- Brenda Flores onkeydown - Cuando un usuario está presionando / manteniendo pulsada una tecla
+
+//2.- Oscar Aguilar onkeyup - Cuando el usuario suelta una tecla
+//3.-Kevin Acosta onmousedown - Cuando se hace clic en el ratón: Alerta qué elemento
+
+//4.-alejandro Grover onmousemove/onmouseout - Al mover el puntero del ratón sobre / fuera de una imagen
+//servido
+//5.- Fidel Aldana Mejia
+//6.- Juan Jose Torrico Salazar
+//7.- Marco Antonio silva
